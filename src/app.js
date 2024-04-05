82a5bc18bd940ae1b30d3e2ebf3065d3beced16f
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-04-05 20:51:06 +0000

    Fixed main to include branch

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#ExcuseBtn").addEventListener("click", () => {
+    document.querySelector("#excuseLine").innerHTML = generateExcuse();
+  });
+  console.log("Excuse");
+};
+let generateExcuse = () => {
+  let who = ["A", "The"];
+  let subject = ["jogger", "racoon", "dog", "driver", "comedian", "pinecone"];
+  let action = [
+    "punched my",
+    "threw my",
+    "crumbled up my",
+    "stole my",
+    "bit my",
+  ];
+  let what = ["homework", "toe", "car", "shoe"];
+  let where = ["in the closet", "under the bed", "in the pool"];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
