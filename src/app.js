c0213f2089589ca142bc2f12425dcf600736e986
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-04-05 21:42:41 +0000

    part 1 of class done with change to function for less and prettier code

--- src/app.js
+++ src/app.js
@@ -14,33 +14,46 @@ window.onload = () => {
   console.log("Excuse");
 };
 let generateExcuse = () => {
-  let who = ["A", "The"];
-  let subject = ["jogger", "racoon", "dog", "driver", "comedian", "pinecone"];
+  let pronoun = ["A", "The"];
+  let who = ["jogger", "racoon", "dog", "driver", "comedian", "pinecone"];
   let action = [
     "punched my",
     "threw my",
     "crumbled up my",
     "stole my",
-    "bit my",
+    "bit my"
   ];
   let what = ["homework", "toe", "car", "shoe"];
   let where = ["in the closet", "under the bed", "in the pool"];
 
-  let whoIndex = Math.floor(Math.random() * who.length);
-  let subjectIndex = Math.floor(Math.random() * subject.length);
-  let actionIndex = Math.floor(Math.random() * action.length);
-  let whatIndex = Math.floor(Math.random() * what.length);
-  let whereIndex = Math.floor(Math.random() * where.length);
+  function selectArrayElement(arrayName, arrayLength) {
+    let randomnumber = Math.floor(Math.random() * arrayLength);
+    let selectedElement = arrayName[randomNumber];
+    return selectedElement;
+  }
+  let randomPronoun = selectArrayElement(pronoun, pronoun.length);
+  let randomWho = selectArrayElement(who, who.length);
+  let randomAction = selectArrayElement(action, action.length);
+  let randomWhat = selectArrayElement(what, what.length);
+  let randomWhere = selectArrayElement(where, where.length);
 
-  return (
-    who[whoIndex] +
-    " " +
-    subject[subjectIndex] +
-    " " +
-    action[actionIndex] +
-    " " +
-    what[whatIndex] +
-    " " +
-    where[whereIndex]
-  );
+  let excuse =
+    "${randomPronoun} ${randomWho} ${randomAction} ${randomWhat} ${randomWhere}";
+  // let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  // let whoIndex = Math.floor(Math.random() * who.length);
+  // let actionIndex = Math.floor(Math.random() * action.length);
+  // let whatIndex = Math.floor(Math.random() * what.length);
+  // let whereIndex = Math.floor(Math.random() * where.length);
+
+  // return (
+  //   pronoun[pronounIndex] +
+  //   " " +
+  //   who[whoIndex] +
+  //   " " +
+  //   action[actionIndex] +
+  //   " " +
+  //   what[whatIndex] +
+  //   " " +
+  //   where[whereIndex]
+  // );
 };
